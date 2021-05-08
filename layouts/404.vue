1e037768fb9e20ab90301aaf178633052a92bb08
zpfz <zpfz@seeyoz.cn>
2021-05-08 17:19:47 +0800

    style: add 404 page's max-width

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="theme-container">
-    <div class="theme-antdocs-content">
+    <div class="theme-antdocs-content page-404">
       <h1>404</h1>
 
       <blockquote>{{ getMsg() }}</blockquote>
@@ -28,3 +28,10 @@ export default {
   }
 }
 </script>
+<style lang="less">
+@import '../styles/palette.less';
+.page-404{
+  max-width: @contentWidthNoSidebar !important;
+  padding: 0 2.5rem !important;
+}
+</style>
